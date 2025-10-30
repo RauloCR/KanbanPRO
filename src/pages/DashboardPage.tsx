38374c59446570dd6452871d714e24efa2e1b1ed
Raúl Orlando Castro Ricaurte <castro_raul159@hotmail.com>
2025-10-30 05:20:06 -0500

    Retomando proyecto...

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Board } from "../features/board/components/Board";
 
-export const DashboardPage = () => {
+const DashboardPage = () => {
     return (
-        <div className="bg-gray-50 min-h-screen">
-            <h1 className="text-x1 font-bold px-6 pt-6">Mi Tablero Kanban</h1>
-            <Board />
+        <div>
+            <h1>Dashboard</h1>
+            {/* Aquí irá la vista tipo Trello */}
         </div>
     );
 };
